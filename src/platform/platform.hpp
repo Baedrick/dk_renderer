4116bef582f6a74977caaa74c532758b2ae7a3cd
Dedrick <dedrickkoh1@gmail.com>
2026-03-13 20:34:08 +0800

    platform file reading interface

--- src/platform/platform.hpp
+++ src/platform/platform.hpp
@@ -3,13 +3,6 @@
 #pragma once
 
 namespace dk {
-	struct PLT_Handle {
-		u64 v;
-
-		auto operator==(PLT_Handle o) const noexcept -> b8;
-		auto operator!=(PLT_Handle o) const noexcept -> b8;
-	};
-
 	struct PLT_SystemInfo {
 		u64 page_size;
 		u32 logical_processor_count;
@@ -17,6 +10,24 @@ namespace dk {
 
 	struct PLT_ProcessInfo {
 		u32 pid;
+	};
+
+	using PLT_AccessFlags = u32;
+	enum : u32 {
+		PLT_ACCESS_FLAG_READ = 1u << 0,
+		PLT_ACCESS_FLAG_WRITE = 1u << 1,
+		PLT_ACCESS_FLAG_APPEND = 1u << 2
+	};
+
+	struct PLT_FileAttributes {
+		u64 size;
+	};
+
+	struct PLT_Handle {
+		u64 v;
+
+		auto operator==(PLT_Handle o) const noexcept -> b8;
+		auto operator!=(PLT_Handle o) const noexcept -> b8;
 	};
 
 	auto plt_handle_invalid() noexcept -> PLT_Handle;
@@ -31,6 +42,9 @@ namespace dk {
 	auto plt_commit(void *ptr, u64 size) noexcept -> b8;
 	auto plt_decommit(void *ptr, u64 size) noexcept -> b8;
 	auto plt_release(void *ptr, u64 size) noexcept -> void;
-	auto plt_malloc(u64 size) noexcept -> void *;
-	auto plt_free(void *ptr) noexcept -> void;
+
+	auto plt_file_open(String8 path, PLT_AccessFlags flags) noexcept -> PLT_Handle;
+	auto plt_file_close(PLT_Handle file) noexcept -> void;
+	auto plt_file_read(PLT_Handle file, u64 begin, u64 end, void *out_data) noexcept -> u64;
+	auto plt_file_write(PLT_Handle file, u64 begin, u64 end, void const *data) noexcept -> u64;
 }
--- src/platform/platform_win32.cpp
+++ src/platform/platform_win32.cpp
@@ -27,7 +27,7 @@ auto dk::plt_get_process_info() noexcept -> PLT_ProcessInfo * {
 }
 
 auto dk::plt_get_entropy(void *data, u64 size) noexcept -> void {
-	DK_ASSERT_ALWAYS(size <= 256); // NOTE(Dedrick): Limit of 256 to follow linux.
+	DK_ASSERT_ALWAYS(size <= 256); // NOTE(Dedrick): Limit of 256 bytes to follow linux.
 	BCryptGenRandom(
 		nullptr,
 		static_cast<u8 *>(data),
@@ -58,12 +58,45 @@ auto dk::plt_release(void *ptr, u64 size) noexcept -> void {
 	VirtualFree(ptr, 0, MEM_RELEASE);
 }
 
-auto dk::plt_malloc(u64 size) noexcept -> void * {
-	return HeapAlloc(GetProcessHeap(), 0, size);
+auto dk::plt_file_open(String8 path, PLT_AccessFlags flags) noexcept -> PLT_Handle {
+	TempArena const scratch = scratch_begin(nullptr, 0);
+	String16 const path16 = str16_from_8(scratch.arena, path);
+
+	DWORD access_flags = 0;
+	DWORD creation_disposition = OPEN_EXISTING;
+	SECURITY_ATTRIBUTES security_attributes = { sizeof(SECURITY_ATTRIBUTES), nullptr, FALSE };
+	if (flags & PLT_ACCESS_FLAG_READ) { access_flags |= GENERIC_READ; }
+	if (flags & PLT_ACCESS_FLAG_WRITE) { access_flags |= GENERIC_WRITE; }
+	if (flags & PLT_ACCESS_FLAG_WRITE) { creation_disposition = CREATE_ALWAYS; }
+	if (flags & PLT_ACCESS_FLAG_APPEND) { creation_disposition = OPEN_ALWAYS; access_flags |= FILE_APPEND_DATA; }
+
+	PLT_Handle result = plt_handle_invalid();
+	HANDLE const file = CreateFileW(
+		reinterpret_cast<WCHAR const *>(path16.data),
+		access_flags,
+		0,
+		&security_attributes,
+		creation_disposition,
+		FILE_ATTRIBUTE_NORMAL,
+		nullptr
+	);
+	if (file != INVALID_HANDLE_VALUE) {
+		result.v = reinterpret_cast<uintptr_t>(file);
+	}
+	scratch_end(scratch);
+	return result;
 }
 
-auto dk::plt_free(void *ptr) noexcept -> void {
-	HeapFree(GetProcessHeap(), 0, ptr);
+auto dk::plt_file_close(PLT_Handle file) noexcept -> void {
+
+}
+
+auto dk::plt_file_read(PLT_Handle file, u64 begin, u64 end, void *out_data) noexcept -> u64 {
+
+}
+
+auto dk::plt_file_write(PLT_Handle file, u64 begin, u64 end, void const *data) noexcept -> u64 {
+
 }
 
 extern auto entry_point(int argc, char **argv) noexcept -> int;
