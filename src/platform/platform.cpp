d6ef1eab1af4b7cea28567f891526ba79a15a816
Dedrick Koh <dedrickkoh1@gmail.com>
2026-03-14 22:06:50 +0800

    remove plt invalid handle from core impl

--- src/platform/platform.cpp
+++ src/platform/platform.cpp
@@ -8,10 +8,6 @@ auto dk::PLT_Handle::operator!=(PLT_Handle o) const noexcept -> b8 {
 	return !(*this == o);
 }
 
-auto dk::plt_handle_invalid() noexcept -> PLT_Handle {
-	return { 0 };
-}
-
 #ifdef DK_PLATFORM_WIN32
 #	include "platform_win32.cpp"
 #endif
