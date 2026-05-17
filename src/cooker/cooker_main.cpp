49c4f4caa945428a0b8f1b8cdb1d9765f3b0a7c0
Dedrick Koh <dedrickkoh1@gmail.com>
2026-05-18 00:54:04 +0800

    add cooker stub

---
+++ src/cooker/cooker_main.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2026 Koh Swee Teck Dedrick. All rights reserved.
+
+#include "base/base.hpp"
+#include "platform/platform.hpp"
+
+#include "base/base.cpp"
+#include "platform/platform.cpp"
+
+auto entry_point(dk::String8List args) noexcept -> int {
+	(void)args;
+	return 0;
+}
