bfaf635de8e27312ea5927c86292d21131063692
Dedrick <dedrickkoh1@gmail.com>
2026-05-21 17:18:50 +0800

    add cooking entry point

---
+++ src/dkcook/dkcook.cpp
@@ -0,0 +1,5 @@
+// Copyright (C) 2026 Koh Swee Teck Dedrick. All rights reserved.
+
+auto dk::dkc_entry_point(CmdLine *cmd_line) noexcept -> void {
+	(void)cmd_line;
+}
---
+++ src/dkcook/dkcook.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2026 Koh Swee Teck Dedrick. All rights reserved.
+
+#pragma once
+
+namespace dk {
+	auto dkc_entry_point(CmdLine *cmd_line) noexcept -> void;
+}
