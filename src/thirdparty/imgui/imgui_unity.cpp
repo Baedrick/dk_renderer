fb9dfa2b262317b5da3653f56b1f09dc9e1b6225
Dedrick Koh <dedrickkoh1@gmail.com>
2026-05-22 00:59:45 +0800

    import imgui docking

---
+++ src/thirdparty/imgui/imgui_unity.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2026 Koh Swee Teck Dedrick. All rights reserved.
+
+#include "imgui.cpp"
+#include "imgui_demo.cpp"
+#include "imgui_draw.cpp"
+#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#include "imgui_impl_opengl3.cpp"
+#include "imgui_tables.cpp"
+#include "imgui_widgets.cpp"
+
+#define RGFW_IMGUI_IMPLEMENTATION
+#include "imgui_impl_rgfw.h"
+#undef RGFW_IMGUI_IMPLEMENTATION
---
+++ src/thirdparty/imgui/imgui_unity.hpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2026 Koh Swee Teck Dedrick. All rights reserved.
+
+#pragma once
+
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_rgfw.h"
