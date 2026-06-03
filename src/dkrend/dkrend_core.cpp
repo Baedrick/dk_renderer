6c8f87b6ab675996c63a4a784beb826a0b3129ac
Dedrick <dedrickkoh1@gmail.com>
2026-06-03 16:59:31 +0800

    set up frame timing and snapping

--- src/dkrend/dkrend_core.cpp
+++ src/dkrend/dkrend_core.cpp
@@ -33,46 +33,120 @@ auto dk::dkr_init(CmdLine *cmd_line) noexcept -> void {
 	dkr_context->window = plt_window_open("RGFW"_str8, 0, 0, 800, 600, RGFW_windowCenter | RGFW_windowScaleToMonitor);
 	rhi_window_equip(dkr_context->window);
 
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.DeltaTime = 1.0f / 60.0f;
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	//~ Dedrick: Pick target frame time.
+	// TODO(Dedrick): Query vsync when window is moved.
+	{
+		f32 target_refresh_rate = 60.0f;
+		RGFW_monitor const *monitor = RGFW_window_getMonitor(dkr_context->window);
+		if (monitor->mode.refreshRate > 0.0f) {
+			target_refresh_rate = monitor->mode.refreshRate;
+		}
+		dkr_context->target_frame_time_us = static_cast<s64>(1e+6f / target_refresh_rate);
+		dkr_context->vsync_max_error_us = 200; // 0.2ms
+		for (u32 i = 0; i < array_count(dkr_context->snap_frequencies); ++i) {
+			dkr_context->snap_frequencies[i] = dkr_context->target_frame_time_us * (i + 1);
+		}
+		for (u32 i = 0; i < array_count(dkr_context->time_averager); ++i) {
+			dkr_context->time_averager[i] = dkr_context->target_frame_time_us;
+		}
+		dkr_context->time_averager_residual = 0;
+	}
 
-	ImGui_ImplRgfw_InitForOpenGL(dkr_context->window, true);
-	ImGui_ImplOpenGL3_Init();
+	//~ Dedrick: Initialize ImGui.
+	{
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO &io = ImGui::GetIO();
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+		ImGui_ImplRgfw_InitForOpenGL(dkr_context->window, true);
+		ImGui_ImplOpenGL3_Init();
+	}
+
+	//~ Dedrick: Begin measuring actual per-frame work.
+	dkr_context->last_frame_time_us = plt_now_microseconds();
 }
 
 auto dk::dkr_frame() noexcept -> b8 {
 	ZoneScoped;
 	TempArena const scratch = scratch_begin(nullptr, 0);
-	log_frame_begin();
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplRgfw_NewFrame();
-	ImGui::NewFrame();
-	ImGui::ShowDemoWindow();
+
+	//~ Dedrick: Determine frame time.
+	// https://medium.com/@tglaiel/how-to-make-your-game-run-at-60fps-24c61210fe75
+	{
+		u64 const current_time_us = plt_now_microseconds();
+		s64 delta_time = static_cast<s64>(current_time_us - dkr_context->last_frame_time_us);
+		dkr_context->last_frame_time_us = current_time_us;
+		if (delta_time > dkr_context->target_frame_time_us * 8) {
+			delta_time = dkr_context->target_frame_time_us;
+		}
+		if (delta_time < 0) {
+			delta_time = 0;
+		}
+		for (u64 i = 0; i < array_count(dkr_context->snap_frequencies); ++i) {
+			s64 const snap = dkr_context->snap_frequencies[i];
+			if (abs(delta_time - snap) < dkr_context->vsync_max_error_us) {
+				delta_time = snap;
+				break;
+			}
+		}
+		u64 const history_count = array_count(dkr_context->time_averager);
+		dkr_context->time_averager[dkr_context->time_averager_head] = delta_time;
+		dkr_context->time_averager_head = (dkr_context->time_averager_head + 1) % history_count;
+
+		u64 averager_sum = 0;
+		for (u32 i = 0; i < array_count(dkr_context->time_averager); ++i) {
+			averager_sum += dkr_context->time_averager[i];
+		}
+
+		delta_time = averager_sum / history_count;
+		dkr_context->time_averager_residual += averager_sum % history_count;
+		delta_time += dkr_context->time_averager_residual / history_count;
+		dkr_context->time_averager_residual %= history_count;
+
+		f32 const frame_dt = static_cast<f32>(delta_time) / 1000000.0f;
+		dkr_context->frame_dt = frame_dt;
+		dkr_context->time_in_seconds += frame_dt;
+	}
 
 	//~ Dedrick: Do per-frame resets.
 	arena_clear(dkr_frame_arena());
 
-	//~ Dedrick: Begin measuring actual per-frame work.
-	u64 const begin_time_us = plt_now_microseconds();
+	//~ Dedrick: Begin frame scope.
+	log_frame_begin();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplRgfw_NewFrame();
+	ImGui::NewFrame();
 
-	// TODO(Dedrick): Process platform (window) events, then process application events.
+	// TODO(Dedrick): Process platform (window) events -> push application events.
 	for (RGFW_event event = {}; RGFW_window_checkEvent(dkr_context->window, &event); ) {
 		if (event.type == RGFW_windowClose) {
 			dkr_context->quit = true;
 		}
 	}
 
-	ImGuiIO &io = ImGui::GetIO();
-	//~ Dedrick: Adjust ImGui for HiDPI screens.
+	// TODO(Dedrick): Process application events -> push asset system events.
+
+	// TODO(Dedrick): Process asset unload events, process asset load events.
+
+	// TODO(Dedrick): Update scene stuff (camera, lights, etc.)
+	DK_LOG_INFOF("frame_dt: %.4f\n", dkr_context->frame_dt);
+
+	//~ Dedrick: Build UI.
 	{
+		ZoneScopedN("build ui");
+		ImGui::ShowDemoWindow();
+
+		//~ Dedrick: Adjust ImGui for HiDPI screens.
+		// TODO(Dedrick): Only adjust when moving window.
+		ImGuiIO &io = ImGui::GetIO();
 		float const old_scale = io.FontGlobalScale;
 		float const content_scale = ImGui_ImplRgfw_GetContentScaleForWindow(dkr_context->window);
 		io.FontGlobalScale = content_scale;
 		ImGui::GetStyle().ScaleAllSizes(content_scale / old_scale);
+
+		ImGui::Render();
 	}
+
 	{
 		s32 width = 0, height = 0;
 		RGFW_window_getSizeInPixels(dkr_context->window, &width, &height);
@@ -83,27 +157,24 @@ auto dk::dkr_frame() noexcept -> b8 {
 	glBindVertexArray(rhi_ogl_context->all_purpose_vao);
 	glUseProgram(rhi_ogl_context->shader);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-
-	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 	rhi_surface_present(dkr_context->window);
 
-	//~ Dedrick: Determine frame time.
-	u64 const end_time_us = plt_now_microseconds();
-	u64 const frame_time_us = end_time_us - begin_time_us;
-	f32 const frame_dt = min(static_cast<f32>(frame_time_us) / 1000000.0f, 0.1f);
-	dkr_context->frame_dt = frame_dt;
-	dkr_context->time_in_seconds += frame_dt;
-
 	//~ Dedrick: Bump frame counters.
 	dkr_context->frame_index += 1;
 
+	//~ Dedrick: Collect logs.
 	{
 		ZoneScopedN("collect logs");
 		LogFrameResult const log = log_frame_end(scratch.arena);
 		// TODO(Dedrick): Log to console buffer.
 		(void)log;
+		if (log.list.count > 0) {
+			for (LogEntry *node = log.list.first; node != nullptr; node = node->next) {
+				std::fprintf(stdout, "%.*s", DK_STR8_VARG(node->string));
+			}
+		}
 	}
 	scratch_end(scratch);
 	return dkr_context->quit;
--- src/dkrend/dkrend_core.hpp
+++ src/dkrend/dkrend_core.hpp
@@ -13,14 +13,26 @@ namespace dk {
 		Arena *arena;
 		b8 quit;
 
+		//~ Dedrick: Log.
 		LogContext *log;
 		String8 log_path;
 
+		//~ Dedrick: Frame history.
 		u64 frame_index;
 		Arena *frame_arenas[2];
 		f64 time_in_seconds;
+
+		//~ Dedrick: Frame timing.
+		u64 last_frame_time_us;
+		s64 target_frame_time_us;
+		s64 vsync_max_error_us;
+		s64 snap_frequencies[8];
+		s64 time_averager[4];
+		s64 time_averager_residual;
+		u32 time_averager_head;
 		f32 frame_dt;
 
+		//~ Dedrick: Window.
 		RGFW_window *window;
 	};
 
