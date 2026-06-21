aaac49f7d28daed2e20fa0f6ede205ffe02c3cf1
Dedrick Koh <dedrickkoh1@gmail.com>
2026-06-21 23:57:36 +0800

    fix linking error for asset engine

--- src/asset_engine/asset_engine.cpp
+++ src/asset_engine/asset_engine.cpp
@@ -1,1 +1,9 @@
 // Copyright (C) 2026 Koh Swee Teck Dedrick. All rights reserved.
+
+auto dk::ase_init(CmdLine *cmd_line) noexcept -> void {
+	(void)cmd_line;
+}
+
+auto dk::ase_shutdown() noexcept -> void {
+
+}
