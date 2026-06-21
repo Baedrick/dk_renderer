febaab00ec88f1226178ab37628d79577f1de82c
Dedrick Koh <dedrickkoh1@gmail.com>
2026-06-21 15:09:59 +0800

    add defer macro

--- src/base/base_core.hpp
+++ src/base/base_core.hpp
@@ -82,6 +82,23 @@ namespace dk {
 	u64 constexpr U64_MAX = 0xFFFFFFFFFFFFFFFF;
 	u64 constexpr U64_MIN = 0;
 
+	template <typename Func>
+	struct defer_impl {
+		defer_impl(Func func) noexcept : func_{ func } { }
+		~defer_impl() { func_(); }
+		defer_impl(defer_impl const &) = delete;
+		auto operator=(defer_impl const &) -> defer_impl & = delete;
+		Func func_;
+	};
+
+	template <typename Func>
+	auto make_defer(Func func) noexcept -> defer_impl<Func> {
+		return defer_impl<Func>(func);
+	};
+
+	#define DK_DEFER_(x) DK_GLUE(x, __COUNTER__)
+	#define DK_DEFER(...) auto DK_DEFER_(_dk_defer_) = make_defer([&](){ __VA_ARGS__; })
+
 	constexpr auto kilo_bytes(u64 x) noexcept -> u64 { return x << 10; }
 	constexpr auto mega_bytes(u64 x) noexcept -> u64 { return x << 20; }
 	constexpr auto giga_bytes(u64 x) noexcept -> u64 { return x << 30; }
