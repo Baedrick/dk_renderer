5ce903ac43e22434d7fede5a1a91783528b898c7
Dedrick Koh <dedrickkoh1@gmail.com>
2026-07-11 21:20:35 +0800

    update dks format for meshlet

--- src/dks/dks.hpp
+++ src/dks/dks.hpp
@@ -11,10 +11,12 @@ namespace dk {
 		DKS_SECTION_KIND_TOP_LEVEL_INFO,
 		DKS_SECTION_KIND_STRING_DATA,
 		DKS_SECTION_KIND_STRING_TABLE,
-		DKS_SECTION_KIND_OBJECTS,
+		DKS_SECTION_KIND_INSTANCES,
+		DKS_SECTION_KIND_GPU_INSTANCES,
 		DKS_SECTION_KIND_GPU_MESHES,
 		DKS_SECTION_KIND_GPU_VERTICES,
 		DKS_SECTION_KIND_GPU_MESHLETS,
+		DKS_SECTION_KIND_GPU_MESHLET_BOUNDS,
 		DKS_SECTION_KIND_GPU_MESHLET_VERTICES,
 		DKS_SECTION_KIND_GPU_MESHLET_TRIANGLES,
 		DKS_SECTION_KIND_COUNT
@@ -41,22 +43,36 @@ namespace dk {
 
 	struct DKS_TopLevelInfo {
 		u32 model_name_string_idx;
-		vec3 dequantization_factor;
-		vec3 dequantization_summand;
 	};
 
-	struct DKS_Object {
+	struct DKS_Instance {
 		u32 name_string_idx;
 	};
 
-	struct DKS_Mesh {
-		f32 center[3];
-		f32 extents[3];
+	struct DKS_GPU_Instance {
+		f32 world_from_object[12]; // mat4x3
+		u32 mesh_idx;
+		u32 pad[3];
+	};
+
+	struct DKS_GPU_Mesh {
+		f32 sphere_center[3];
+		f32 sphere_radius;
+		f32 dequantization_factor[3];
+		f32 dequantization_summand[3];
 		u32 meshlet_offset;
 		u32 meshlet_count;
 	};
 
-	struct DKS_Meshlet {
+	struct DKS_GPU_MeshletBounds {
+		f32 sphere_center[3];
+		f32 sphere_radius;
+		f32 cone_apex[3];
+		f32 cone_axis[3];
+		f32 cone_cutoff;
+	};
+
+	struct DKS_GPU_Meshlet {
 		u32 vertex_offset;
 		u32 triangle_offset;
 		u8 vertex_count;
@@ -64,17 +80,19 @@ namespace dk {
 		u16 pad;
 	};
 
-	struct DKS_Vertex {
+	struct DKS_GPU_Vertex {
 		u64 position; ///< fixed point quantized: [unused:1][x:21][y:21][z:21]
 	};
 
 	using DKS_SectionElementType_TopLevelInfo         = DKS_TopLevelInfo;
 	using DKS_SectionElementType_StringData           = u8;
 	using DKS_SectionElementType_StringTable          = DKS_StringTable;
-	using DKS_SectionElementType_Objects              = DKS_Object;
-	using DKS_SectionElementType_GPU_Meshes           = DKS_Mesh;
-	using DKS_SectionElementType_GPU_Meshlets         = DKS_Meshlet;
-	using DKS_SectionElementType_GPU_Vertices         = DKS_Vertex;
+	using DKS_SectionElementType_Instances            = DKS_Instance;
+	using DKS_SectionElementType_GPU_Instances        = DKS_GPU_Instance;
+	using DKS_SectionElementType_GPU_Meshes           = DKS_GPU_Mesh;
+	using DKS_SectionElementType_GPU_Vertices         = DKS_GPU_Vertex;
+	using DKS_SectionElementType_GPU_Meshlets         = DKS_GPU_Meshlet;
+	using DKS_SectionElementType_GPU_MeshletBounds    = DKS_GPU_MeshletBounds;
 	using DKS_SectionElementType_GPU_MeshletVertices  = u32;
 	using DKS_SectionElementType_GPU_MeshletTriangles = u32; // packed: [i3:8][i2:8][i1:8][i0:8]
 
@@ -82,10 +100,12 @@ namespace dk {
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_TOP_LEVEL_INFO>        { using Type = DKS_SectionElementType_TopLevelInfo; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_STRING_DATA>           { using Type = DKS_SectionElementType_StringData; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_STRING_TABLE>          { using Type = DKS_SectionElementType_StringTable; };
-	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_OBJECTS>               { using Type = DKS_SectionElementType_Objects; };
+	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_INSTANCES>             { using Type = DKS_SectionElementType_Instances; };
+	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_INSTANCES>         { using Type = DKS_SectionElementType_GPU_Instances; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_MESHES>            { using Type = DKS_SectionElementType_GPU_Meshes; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_VERTICES>          { using Type = DKS_SectionElementType_GPU_Vertices; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_MESHLETS>          { using Type = DKS_SectionElementType_GPU_Meshlets; };
+	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_MESHLET_BOUNDS>    { using Type = DKS_SectionElementType_GPU_MeshletBounds; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_MESHLET_VERTICES>  { using Type = DKS_SectionElementType_GPU_MeshletVertices; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_MESHLET_TRIANGLES> { using Type = DKS_SectionElementType_GPU_MeshletTriangles; };
 
