7e03a9bdc689ece208e10da3c1a92831b41e2f5b
Dedrick Koh <dedrickkoh1@gmail.com>
2026-08-20 16:29:01 +0800

    change dks format to support multimesh nodes and empty nodes on cpu side

--- src/dks/dks.hpp
+++ src/dks/dks.hpp
@@ -11,7 +11,8 @@ namespace dk {
 		DKS_SECTION_KIND_TOP_LEVEL_INFO,
 		DKS_SECTION_KIND_STRING_DATA,
 		DKS_SECTION_KIND_STRING_TABLE,
-		DKS_SECTION_KIND_INSTANCES,
+		DKS_SECTION_KIND_NODES,
+		DKS_SECTION_KIND_GPU_TRANSFORMS,
 		DKS_SECTION_KIND_GPU_INSTANCES,
 		DKS_SECTION_KIND_GPU_MESHES,
 		DKS_SECTION_KIND_GPU_VERTICES,
@@ -28,7 +29,7 @@ namespace dk {
 		u32 section_offset;
 		u32 section_count;
 		u32 pad;
-		u64 metadata_size; //< CPU data size.
+		u64 cpu_data_size;
 	};
 
 	struct DKS_Section {
@@ -45,18 +46,26 @@ namespace dk {
 		u32 model_name_string_idx;
 	};
 
-	struct DKS_Instance {
+	struct DKS_Node {
 		u32 name_string_idx;
 		u32 parent;
 		u32 first_child;
 		u32 prev_sibling;
 		u32 next_sibling;
+		u32 gpu_transform_idx;
 		u32 gpu_instance_offset;
 		u32 gpu_instance_count;
+		vec3 local_translation;
+		quat local_rotation;
+		vec3 local_scale;
+	};
+
+	struct DKS_GPU_Transform {
+		f32 world_from_object[12]; ///< mat4x3, implicit vec4(0,0,0,1)
 	};
 
 	struct DKS_GPU_Instance {
-		f32 world_from_object[12]; ///< mat4x3, implicit vec4(0,0,0,1)
+		u32 gpu_transform_idx;
 		u32 mesh_idx;
 	};
 
@@ -92,7 +101,8 @@ namespace dk {
 	using DKS_SectionElementType_TopLevelInfo         = DKS_TopLevelInfo;
 	using DKS_SectionElementType_StringData           = u8;
 	using DKS_SectionElementType_StringTable          = DKS_StringTable;
-	using DKS_SectionElementType_Instances            = DKS_Instance;
+	using DKS_SectionElementType_Nodes                = DKS_Node;
+	using DKS_SectionElementType_GPU_Transforms       = DKS_GPU_Transform;
 	using DKS_SectionElementType_GPU_Instances        = DKS_GPU_Instance;
 	using DKS_SectionElementType_GPU_Meshes           = DKS_GPU_Mesh;
 	using DKS_SectionElementType_GPU_Vertices         = DKS_GPU_Vertex;
@@ -105,7 +115,8 @@ namespace dk {
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_TOP_LEVEL_INFO>        { using Type = DKS_SectionElementType_TopLevelInfo; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_STRING_DATA>           { using Type = DKS_SectionElementType_StringData; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_STRING_TABLE>          { using Type = DKS_SectionElementType_StringTable; };
-	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_INSTANCES>             { using Type = DKS_SectionElementType_Instances; };
+	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_NODES>                 { using Type = DKS_SectionElementType_Nodes; };
+	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_TRANSFORMS>        { using Type = DKS_SectionElementType_GPU_Transforms; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_INSTANCES>         { using Type = DKS_SectionElementType_GPU_Instances; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_MESHES>            { using Type = DKS_SectionElementType_GPU_Meshes; };
 	template <> struct DKS_SectionTraits<DKS_SECTION_KIND_GPU_VERTICES>          { using Type = DKS_SectionElementType_GPU_Vertices; };
